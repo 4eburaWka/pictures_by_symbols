d0b409543eefb6329f06c210ee350c28f0c5f856
4eburaWka <prikpasa14@gmail.com>
2022-07-14 20:09:44 +0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,1 +1,55 @@
+from PIL import Image, ImageDraw, ImageFont
+import numpy as np  
 
+
+path = 'E:\\тима.jpg'
+symbols = " .:!/r(l1Z4H9W8$@" # 17
+
+def Image_To_Text(path : str, size=175, symbols=symbols) -> str:
+    """You put path or link to your image that is SQUARE, 
+    size(length) of line that you want to photo be, 
+    list of symbols sorted by brightness 
+    and get your photo by symbols"""
+
+    with Image.open(path) as img:
+        img.thumbnail((size,size))
+        arr = np.asarray(img)
+    text = ''
+
+    for x in range(0, size, 2):
+        
+        for y in range(size):
+            RGB = 0             # 765 max
+            for canal in arr[x][y]:
+                RGB += canal
+            text += symbols[RGB // (765 // len(symbols))]
+        text += '\n'
+    return text
+
+text = Image_To_Text('E:\\dada.jpg')
+
+def Text_To_Image_by_text(text, img):
+    font = ImageFont.truetype('C:\Windows\Fonts\CONSOLA.TTF', size=10)
+    ImageDraw.Draw(img).text((0, 0), text, font=font, fill=('#F5F5F5'))
+
+
+img = Image.new('RGB', (1050,1040), color=('#000000'))
+Text_To_Image_by_text(text, img)
+img.show(text)
+
+# arr=text.split('\n')
+# # arr=[[] for x in range(100)]
+# # for x in range(100):
+# #     for y in range(100):
+
+# #         arr1[x].append()
+# colors = [[] for x in range(100)]
+# try:
+#     for x in range(100):
+#         color = symbols.count(arr[x][y]) * 45
+#         colors[x].append([color, color,color])
+# except:
+#     True
+
+# img2 = Image.fromarray(np.uint8(colors)).convert('RGB')
+# img2.show()
